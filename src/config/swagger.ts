9d23890783e6ada78fef61cab5c4876089badcfa
shivam-shukla-25 <shivamshdev@gmail.com>
2025-11-23 12:25:52 +0530

    Fix TS build errors for Render deployment

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,5 +1,5 @@
 import swaggerJsdoc from "swagger-jsdoc";
-import path from "node:path";
+import path from "path";
 
 export const swaggerSpec = swaggerJsdoc({
   definition: {
@@ -9,7 +9,7 @@ export const swaggerSpec = swaggerJsdoc({
       version: "1.0.0",
       description: "API documentation for Voucher System",
     },
-    servers: [{ url: "http://localhost:5000" }],
+    servers: [{ url: process.env.BASE_URL || "http://localhost:5000" }],
   },
   apis: [path.join(__dirname, "../../swagger.yaml")],
 });
