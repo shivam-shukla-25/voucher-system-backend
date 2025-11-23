4669a04ad4be7b224ba28e97607c67525b2f52fa
shivam-shukla-25 <shivamshdev@gmail.com>
2025-11-23 14:11:20 +0530

    Add ObjectId validation middleware to apply routes

--- src/routes/apply.routes.ts
+++ src/routes/apply.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import applyController from "../controllers/apply.controller";
+import { validateObjectId } from "../middleware/validateObjectId";
 
 const router = Router();
 
-router.post("/", applyController.apply);
+router.post("/", validateObjectId, applyController.apply);
 
 export default router;
