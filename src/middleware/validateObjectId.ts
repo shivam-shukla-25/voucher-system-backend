fb7e9c29e9752c88a122b544d0c88f94c3475d9b
shivam-shukla-25 <shivamshdev@gmail.com>
2025-11-23 13:52:11 +0530

    Add ObjectId validation middleware to order routes

---
+++ src/middleware/validateObjectId.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+import mongoose from "mongoose";
+
+export function validateObjectId(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const id = req.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({
+      status: "error",
+      message: "Invalid ID format",
+    });
+  }
+
+  next();
+}
--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import orderController from "../controllers/order.controller";
+import { validateObjectId } from "../middleware/validateObjectId";
 
 const router = Router();
 
 router.post("/", orderController.create);
 router.get("/", orderController.list);
-router.get("/:id", orderController.get);
+router.get("/:id", validateObjectId, orderController.get);
 
 export default router;
